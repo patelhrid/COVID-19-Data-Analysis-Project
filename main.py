99e9eb0d76df1c88a132e4676d58afc7272c85bb
Hrid Patel <74324750+patelhrid@users.noreply.github.com>
2021-12-11 00:21:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ program. This module will load the necessary files from the datasets, perform co
 the data, and produce an interactive graph in your browser. (SUBJECT TO CHANGE)
 """
 
-import math
-import scrapy
+from countries import Country
+from factors import get_food_insecurity
 
+# Since scrapy can only run once
+all_food_insecurity = get_food_insecurity()
+
+# All countries as Country objects, with their respective statistics
+CANADA = Country('Canada', all_food_insecurity)
+USA = Country('United States', all_food_insecurity)
+UAE = Country('United Arab Emirates', all_food_insecurity)
+FRANCE = Country('France', all_food_insecurity)
+CHINA = Country('China', all_food_insecurity)
+JAPAN = Country('Japan', all_food_insecurity)
+AUSTRALIA = Country('Australia', all_food_insecurity)
+UK = Country('United Kingdom', all_food_insecurity)
