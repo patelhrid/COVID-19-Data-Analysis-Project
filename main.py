cf7580ec66f0bfa7648da162ba7dcb96667292e4
Hrid Patel <74324750+patelhrid@users.noreply.github.com>
2021-12-11 00:37:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ the data, and produce an interactive graph in your browser. (SUBJECT TO CHANGE)
 """
 
 from countries import Country
-from factors import get_food_insecurity
+from factors import get_food_insecurity, get_confirmed_cases
+import plotly.graph_objects as go
 
 # Since scrapy can only run once
 all_food_insecurity = get_food_insecurity()
@@ -23,3 +24,121 @@ CHINA = Country('China', all_food_insecurity)
 JAPAN = Country('Japan', all_food_insecurity)
 AUSTRALIA = Country('Australia', all_food_insecurity)
 UK = Country('United Kingdom', all_food_insecurity)
+
+COUNTRIES = [CANADA, USA, UAE, FRANCE, CHINA, JAPAN, AUSTRALIA, UK]
+
+
+def data_cases_vs_fi() -> dict[str, tuple[float, float]]:
+    """Return the data for the plot graphing confirmed cases vs food insecurity levels for each
+    country.
+
+    Preconditions
+        - this function is only run once
+    """
+    food_insecurity = all_food_insecurity
+    confirmed_cases = get_confirmed_cases()
+
+    # ACCUMULATOR data_so_far: the data of confirmed cases and food insecurity for each country,
+    # so far
+    data_so_far = {}
+    countries = [key for key in food_insecurity]
+
+    for country in countries:
+        if country in confirmed_cases:
+            x, y = confirmed_cases[country], food_insecurity[country]
+            data_so_far[country] = (x, y)
+
+    return data_so_far
+
+
+def plot_cases_vs_fi() -> None:
+    """Graph the data for confirmed cases to food insecurity."""
+    data = data_cases_vs_fi()
+    x_coords = []
+    y_coords = []
+    countries = []
+
+    for country in data:
+        x_coords.append(data[country][0] / 100)
+        y_coords.append(data[country][1] / 100)
+        countries.append(country)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=x_coords,
+        y=y_coords,
+        mode='markers',
+        text=countries,
+        hovertemplate='<b>%{text}</b><br><br>' +
+                      'Confirmed Cases: %{x:.2%}<br>' +
+                      'Food Insecurity: %{y:.1%}<br>' +
+                      '<extra></extra>'
+    ))
+
+    fig.update_layout(title={'text': 'Confirmed COVID-19 Cases vs Food Insecurity',
+                             'y': 0.92, 'x': 0.5, 'xanchor': 'center', 'yanchor': 'top'},
+                      xaxis_title='Confirmed Cases (%)',
+                      yaxis_title='Food Insecurity (%)')
+
+    fig.show()
+
+
+def plot_cases_vs_fi_countries() -> None:
+    """Graph the data for confirmed cases to food insecurity for specific countries."""
+    data = data_cases_vs_fi()
+    values = []
+
+    # for country in COUNTRIES:
+    #     x_values.append(data[country][0])
+    #     y_values.append(data[country][1])
+    #     countries_so_far.append(country)
+
+    for country in COUNTRIES:
+        x, y, c = data[country.name][0], data[country.name][1], country.name
+        values.append((x, y, c))
+
+    values.sort()
+
+    x_values = [value[0] / 100 for value in values]
+    y_values = [value[1] / 100 for value in values]
+    countries_so_far = [value[2] for value in values]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=x_values,
+        y=y_values,
+        mode='lines+markers',
+        text=countries_so_far,
+        hovertemplate='<b>%{text}</b><br><br>' +
+                      'Confirmed Cases: %{x:.2%}<br>' +
+                      'Food Insecurity: %{y:.1%}<br>' +
+                      '<extra></extra>'
+    ))
+
+    fig.update_layout(title={'text': 'Confirmed COVID-19 Cases vs Food Insecurity',
+                             'y': 0.92, 'x': 0.5, 'xanchor': 'center', 'yanchor': 'top'},
+                      xaxis_title='Confirmed Cases (%)',
+                      yaxis_title='Food Insecurity (%)')
+
+    fig.show()
+
+
+# if __name__ == '__main__':
+    # plot_cases_vs_fi()
+    # plot_cases_vs_fi_countries()
+    # import python_ta
+    #
+    # python_ta.check_all(config={
+    #     'max-line-length': 100,
+    #     'extra-imports': ['python_ta.contracts', 'factors'],
+    #     'disable': ['R1705', 'C0200', 'E9998', 'E9999'],
+    # })
+    #
+    # import python_ta.contracts
+    #
+    # python_ta.contracts.DEBUG_CONTRACTS = False
+    # python_ta.contracts.check_all_contracts()
+    #
+    # import doctest
+    #
+    # doctest.testmod()
