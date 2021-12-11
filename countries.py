fd21967acc981a389e2d138332a922951115dccf
Hrid Patel <74324750+patelhrid@users.noreply.github.com>
2021-12-10 23:41:38 -0500

    Update countries.py

--- countries.py
+++ countries.py
@@ -9,7 +9,7 @@ the data, and produce an interactive graph in your browser. (SUBJECT TO CHANGE)
 """
 
 import csv
-from factors import ConfirmedCases, Unemployment, unemployment
+import factors
 
 
 class Country:
@@ -28,66 +28,20 @@ class Country:
     name: str
     population: int
     food_insecurity: float
-    confirmed_cases: ConfirmedCases()
+    confirmed_cases: float
     unemployment: float
     cpi: float
     income: float
-    # conversion:
 
     def __init__(self, name: str) -> None:
         """Initialize the attributes of Country."""
         self.name = name
         self.population = ppln(self.name)
-        self.food_insecurity = 0
-        self.confirmed_cases = ConfirmedCases()
-        self.unemployment = unemployment(self.name)
-        self.cpi = 0
-        self.income = 0
-        # self.conversion = ...
-
-        self.confirmed_cases.calculate_percent(self.name, self.population)
-
-    # def calculate_confirmed_cases(self) -> None:
-    #     """Update the amount of confirmed cases of the country, as a percentage."""
-    #     self.confirmed_cases.calculate_percent(self.name, self.population)
-
-
-# remove
-class Canada(Country):
-    """A concrete class representing the country Canada.
-
-    Instance Attributes:
-        - name: the name of the country
-        - population: the total population of the country
-        - food_insecurity: the percent of the country's population that is food insecure
-        - confirmed cases: the amount of confirmed COVID-19 cases as a percent of the population
-        - unemployment: the unemployment rate of the country, as a percentage
-        - cpi: the consumer price index for food, as a percentage
-        - income: the average income of the country
-    """
-    name: str
-    population: int
-    food_insecurity: float
-    confirmed_cases: ConfirmedCases()
-    unemployment: float
-    cpi: float
-    income: float
-
-    def __init__(self) -> None:
-        """Initialize the attributes of Canada."""
-        self.name = 'Canada'
-        Country.__init__(self, self.name)
-        self.food_insecurity = 0
-        self.confirmed_cases = ConfirmedCases()
-        self.unemployment = 0
-        self.cpi = 0
-        self.income = 0
-
-        self.calculate_confirmed_cases()
-
-    def calculate_confirmed_cases(self) -> None:
-        """Update the amount of confirmed cases of the country, as a percentage."""
-        self.confirmed_cases.calculate_percent(self.name, self.population)
+        self.food_insecurity = factors.get_food_insecurity()[self.name]
+        self.confirmed_cases = factors.get_confirmed_cases()[self.name]
+        self.unemployment = factors.get_unemployment(self.name)
+        self.cpi = factors.get_cpi_average(self.name)
+        self.income = factors.get_income(self.name)
 
 
 def ppln(country: str) -> int:
@@ -109,9 +63,6 @@ def ppln(country: str) -> int:
                 total_population = int(row[-2])
 
     return total_population
-
-
-
 
 # if __name__ == '__main__':
 #     import python_ta
