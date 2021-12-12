0a0c7ef834be59a7c83b15b960591a7a7e9c68ef
cassandratin <95544968+cassandratin@users.noreply.github.com>
2021-12-12 18:25:43 -0500

    Update countries.py

--- countries.py
+++ countries.py
@@ -3,17 +3,15 @@
 Instructions
 ============
 
-This Python module contains the main functions and blocks of code required to run the entire
-program. This module will load the necessary files from the datasets, perform computations on
-the data, and produce an interactive graph in your browser. (SUBJECT TO CHANGE)
+This Python module contains a class representing a country with several instance attributes
+representing the different factors we will be analyzing.
 """
-
-import csv
-import factors
+from factors import ppln, get_unemployment, get_cpi, get_income_usd, get_confirmed_cases, \
+     FoodInsecurity
 
 
 class Country:
-    """A class representing a country, along with several factors related ot the
+    """A class representing a country, along with several factors related to the
     pandemic.
 
     Instance Attributes:
@@ -22,8 +20,26 @@ class Country:
         - food_insecurity: the percent of the country's population that is food insecure
         - confirmed cases: the amount of confirmed COVID-19 cases as a percent of the population
         - unemployment: the unemployment rate of the country, as a percentage
-        - cpi: the consumer price index for food, as a percentage
-        - income: the average income of the country
+        - cpi: the consumer price index for food
+        - income: the average income of the country, in USD
+
+    Representation Invariants:
+        - self.name != ''
+        - self.population >= 0
+        - 0 <= self.food_insecurity <= 100
+        - 0 <= self.confirmed_cases <= 100
+        - 0 <= self.unemployment <= 100
+        - self.cpi >= 0
+        - self.income >= 0
+
+    Sample Usage:
+    >>> fi = FoodInsecurity()
+    >>> fi.percentages = {'Canada': 22.0}
+    >>> canada = Country('Canada', fi)
+    >>> canada.name
+    'Canada'
+    >>> canada.food_insecurity
+    22.0
     """
     name: str
     population: int
@@ -33,58 +49,31 @@ class Country:
     cpi: float
     income: float
 
-    def __init__(self, name: str, food_insecurity_dict: dict) -> None:
+    def __init__(self, name: str, fi: FoodInsecurity) -> None:
         """Initialize the attributes of Country."""
         self.name = name
         self.population = ppln(self.name)
-        self.food_insecurity = food_insecurity_dict[self.name]
-        self.confirmed_cases = factors.get_confirmed_cases()[self.name]
-        self.unemployment = factors.get_unemployment(self.name)
-        self.cpi = factors.get_cpi_average(self.name)
-        self.income = factors.get_income(self.name)
+        self.food_insecurity = fi.percentages[self.name]
+        self.confirmed_cases = get_confirmed_cases()[self.name]
+        self.unemployment = get_unemployment(self.name)
+        self.cpi = get_cpi(self.name)
+        self.income = get_income_usd(self.name)
 
 
-def ppln(country: str) -> int:
-    """Return the population of the country from a dataset.
+if __name__ == '__main__':
+    import python_ta
 
-    # Preconditions:
-    #     - the second last row for each country is the population in 2020
-    """
-    filename = 'datasets/API_SP.POP.TOTL_DS2_en_csv_v2_3358390.csv'
-    total_population = 0
-    with open(filename) as f:
-        # skip the first 5 lines
-        reader = csv.reader(f, delimiter=',')
-        _ = [next(reader) for _ in range(5)]
+    python_ta.check_all(config={
+        'max-line-length': 100,
+        'extra-imports': ['python_ta.contracts', 'factors'],
+        'disable': ['R1705', 'C0200', 'E9998', 'E9999'],
+    })
 
-        for row in reader:
-            current_country = row[0]
-            if country in current_country:
-                total_population = int(row[-2])
-            elif (country == 'United States'
-                  or country == 'United States of America'
-                  or country == 'USA') \
-                    and (row[1] == 'United States'
-                         or row[1] == 'United States of America'
-                         or row[1] == 'USA'):
-                total_population = int(row[-2])
+    import python_ta.contracts
 
-    return total_population
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
 
-# if __name__ == '__main__':
-#     import python_ta
-#
-#     python_ta.check_all(config={
-#         'max-line-length': 100,
-#         'extra-imports': ['python_ta.contracts', 'factors'],
-#         'disable': ['R1705', 'C0200', 'E9998', 'E9999'],
-#     })
-#
-#     import python_ta.contracts
-#
-#     python_ta.contracts.DEBUG_CONTRACTS = False
-#     python_ta.contracts.check_all_contracts()
-#
-#     import doctest
-#
-#     doctest.testmod()
+    import doctest
+
+    doctest.testmod()
