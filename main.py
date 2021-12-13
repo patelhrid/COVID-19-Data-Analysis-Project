5d593007ab2b0b62bb8be6214653479a7f119299
cassandratin <95544968+cassandratin@users.noreply.github.com>
2021-12-13 00:25:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 Instructions
 ============
 
-This Python module contains the main functions and blocks of code required to run the entire
-program. This module will load the necessary files from the datasets, perform computations on
-the data, and produce interactive graphs in your browser.
+This Python module contains the main function and blocks of code required to run the entire
+program. When this module is run, the food_security.json file is created from the factors module.
+Call the show_graph function to produce interactive graphs in your browser of food insecurity,
+confirmed COVID-19 cases, and the potential factor influencing food insecurity!
 """
 
 from scrapy.crawler import CrawlerProcess
@@ -13,7 +14,7 @@ from factors import FoodSecurity, FoodInsecurity
 import graphs
 
 
-def run_graph() -> None:
+def show_graph() -> None:
     """Generate a graph showing the food insecurity, confirmed cases, and different factors."""
     graphs.plot_graph(food_insecurity)
 
@@ -27,3 +28,20 @@ if __name__ == '__main__':
 
     food_insecurity = FoodInsecurity()
     food_insecurity.calculate_food_insecurity()
+
+    import python_ta
+
+    python_ta.check_all(config={
+        'extra-imports': ['python_ta.contracts', 'scrapy.crawler', 'factors', 'graphs'],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200'],
+    })
+
+    import python_ta.contracts
+
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
+
+    import doctest
+
+    doctest.testmod()
