d16f41220be69cbf008aae2aa510a5f03d6f33e0
cassandratin <95544968+cassandratin@users.noreply.github.com>
2021-12-13 20:33:21 -0500

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,11 +1,11 @@
-"""Factors that Affect Food Insecurity or are Affected by COVID-19
+"""Factors Affectd by COVID-19 that Influence Food Insecurity
 
 Description
 ============
 
 This Python module contains the functions and classes that extract and perform any necessary
-calculations on the data from several files for food insecurity levels, confirmed cases,
-unemployment rate, income levels, and consumer price index for multiple countries.
+calculations on the data for food insecurity levels, confirmed cases, unemployment rate,
+income levels, and consumer price index for multiple countries.
 """
 import csv
 import json
@@ -24,7 +24,7 @@ class IncorrectCountryError(Exception):
 
 class FoodSecurity(Spider):
     """A spider that inherits from the scrapy.Spider class and extracts data of food security
-    levels in 2020 as a percentage.
+    levels in 2020 as an index.
 
     Instance Attributes:
         - name: the name of the spider class
@@ -41,6 +41,7 @@ class FoodSecurity(Spider):
     def start_requests(self) -> None:
         """Generate Request objects from the given urls, and instantiate Response objects to be
         passed as arguments to the parse method."""
+        # The specific request url
         urls = [
             'https://impact.economist.com/sustainability/project/food-security-index/Index/YoY'
         ]
@@ -49,39 +50,38 @@ class FoodSecurity(Spider):
             yield Request(url=url, callback=self.parse)
 
     def parse(self, response: scrapy.http.Response, **kwargs) -> None:
-        """Generates dictionaries mapping countries to food security levels in 2020."""
+        """Generates dictionaries mapping countries to food security indices in 2020."""
         for row in response.xpath('//*[@id="yeartoyear"]//tbody/tr'):
             yield {row.xpath('td//text()')[1].get(): row.xpath('td//text()')[10].get()}
 
 
 class FoodInsecurity:
-    """A class for the amount of food insecurity of a country.
+    """A class for the food insecurity index of a country.
 
     Instance Attributes:
-        - percentages: a dictionary mapping countries to food insecurity level as a percent
+        - index: a dictionary mapping countries to food insecurity index
 
     Representation Invariants:
-        - all(self.percentages[country] >= 0 for country in self.percentages)
+        - all(self.index[country] >= 0 for country in self.index)
     """
-    percentages: dict[str, float]
+    index: dict[str, float]
 
     def __init__(self) -> None:
-        """Initiate the percentages attribute of this class as an empty dictionary."""
-        self.percentages = {}
+        """Initiate the index attribute of this class as an empty dictionary."""
+        self.index = {}
 
     def calculate_food_insecurity(self) -> None:
-        """Update the food insecurity levels of each country in 2020 as a percentage.
+        """Update the food insecurity index of each country in 2020.
 
         Preconditions:
             - the file 'food_security.json' created from FoodSecurity is in the same directory \
                 as 'main.py'
 
-        Sample Usage:
         >>> fi = FoodInsecurity()
         >>> fi.calculate_food_insecurity()
-        >>> len(fi.percentages)
+        >>> len(fi.index)
         113
-        >>> fi.percentages['Canada']
+        >>> fi.index['Canada']
         22.0
         """
         # Open the json file created from the scrapy spider (FoodSecurity)
@@ -89,10 +89,10 @@ class FoodInsecurity:
         with open(filename) as json_file:
             data = json.load(json_file)
 
-        # Convert food security levels to food insecurity levels by subtracting each value from 100
+        # Convert food security index to food insecurity index by subtracting each value from 100
         for pair in data:
             country = list(pair.keys())[0]
-            self.percentages[country] = round(100 - float(pair[country]), 1)
+            self.index[country] = round(100 - float(pair[country]), 1)
 
 
 def get_unemployment(country: str) -> float:
@@ -106,6 +106,8 @@ def get_unemployment(country: str) -> float:
     """
     filename = 'datasets/unemployment.csv'
 
+    # 'South Korea' is stored in this dataset as 'Korea, Rep.', so reassign country to match the
+    # string in the dataset and return the correct value
     if country == 'South Korea':
         country = 'Korea, Rep.'
 
@@ -113,8 +115,9 @@ def get_unemployment(country: str) -> float:
     unemployment_rate = 0
 
     with open(filename) as f:
-        # Skip the first 5 lines
         reader = csv.reader(f, delimiter=',')
+
+        # Skip the first 5 lines
         _ = [next(reader) for _ in range(5)]
 
         for row in reader:
@@ -130,7 +133,7 @@ def get_unemployment(country: str) -> float:
 
 
 def get_income_usd(country: str) -> float:
-    """Return the income per capita of country in US Dollars in 2020.
+    """Return the average annual income of country in 2020, in US Dollars.
 
     Preconditions:
         - country is a valid country name
@@ -138,11 +141,15 @@ def get_income_usd(country: str) -> float:
     >>> get_income_usd('Canada')
     56674.16
     """
-    # Return the income level of the USA in USD, without needing to convert the value
+    # A list of countries in the Euro Zone, who use Euros as their currency (country name in the
+    # dataset would be 'Euro Zone')
     euro_zone = ['Belgium', 'Germany', 'Ireland', 'Spain', 'France', 'Italy', 'Luxembourg',
                  'Netherlands', 'Austria', 'Portugal', 'Finland', 'Greece', 'Slovenia', 'Cyprus',
                  'Malta', 'Slovakia', 'Estonia', 'Latvia', 'Lithuania']
 
+    # Return the income level of the USA in USD, without needing to convert the value
+    # 'South Korea' is stored in this dataset as 'Korea', so reassign country to match the string
+    # in the dataset and return the correct value
     if country == 'United States':
         return get_income(country)
     elif country in euro_zone:
@@ -167,7 +174,7 @@ def get_income_usd(country: str) -> float:
 
 
 def get_income(country: str) -> float:
-    """Return the average annual income per captia of country in 2020.
+    """Return the average annual income of country in 2020, in country's currency.
 
     Preconditions:
         - country is a valid country name
@@ -176,6 +183,7 @@ def get_income(country: str) -> float:
     72259.55
     """
     filename = 'datasets/income.csv'
+
     with open(filename) as file:
         reader = csv.reader(file)
 
@@ -203,6 +211,8 @@ def get_cpi(country: str) -> float:
     """
     filename = 'datasets/cpi.csv'
 
+    # 'South Korea' is stored in this dataset as 'Republic of Korea', so reassign country to
+    # match the string in the dataset and return the correct value
     if country == 'South Korea':
         country = 'Republic of Korea'
 
@@ -212,14 +222,14 @@ def get_cpi(country: str) -> float:
         # Skip the header
         next(reader)
 
-        # ACCUMULATOR cpi_value_so_far: keeps track of each month's cpi value for a country.
+        # ACCUMULATOR cpi_value_so_far: keeps track of each month's CPI value for a country
         cpi_value_so_far = []
 
         for row in reader:
             current_country = row[3]
             cpi_value = row[11]
-            # 'United States' and is stored in this dataset as 'United States of America', so
-            # check if country is in current_country (similar issue for 'China')
+            # 'United States' is stored in this dataset as 'United States of America', so
+            # check if country is in current_country
             if country in current_country:
                 cpi_value_so_far.append(float(cpi_value))
 
@@ -227,7 +237,7 @@ def get_cpi(country: str) -> float:
         if cpi_value_so_far == []:
             raise IncorrectCountryError
 
-        # Calculate the mean of all the cpi values over 12 months
+        # Calculate the mean of all the CPI values over 12 months
         average_value = statistics.mean(cpi_value_so_far)
         return round(average_value, 2)
 
@@ -271,6 +281,8 @@ def ppln(country: str) -> int:
     """
     filename = 'datasets/population.csv'
 
+    # 'South Korea' is stored in this dataset as 'Korea, Rep', so reassign country to match the
+    # string in the dataset and return the correct value
     if country == 'South Korea':
         country = 'Korea, Rep.'
 
